11900a5ac18b4078150dda5ec468672155c4852d
wangdefa <249558764@qq.com>
2019-06-17 18:43:32 +0800

    设置

--- settings.py
+++ settings.py
@@ -8,4 +8,4 @@ class Settings():
         self.screen_height = 800
         self.bg_color = (230, 230, 230)
         # 飞船的设置
-        self.ship_speed_factor = 1.5
+        self.ship_speed_factor = 1.6
