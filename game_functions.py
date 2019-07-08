0c8b8a9787deb889cfa04bf5da3af5c3b6f2d12f
wangdefa <249558764@qq.com>
2019-07-08 21:36:17 +0800

    等待记分模块到来

--- game_functions.py
+++ game_functions.py
@@ -1,5 +1,5 @@
 import sys
-
+from time import sleep
 import pygame
 from bullet import Bullet
 from alien import Alien
@@ -133,7 +133,37 @@ def change_fleet_direction(ai_settings, aliens):
     ai_settings.fleet_direction *= -1
 
 
-def update_aliens(ai_settings, aliens):
+def ship_hit(ai_settings, stats, screen, ship, aliens, bullets):
+    """响应被外星人撞到的飞船"""
+    if stats.ships_left >0:
+        #将ships_left减1
+        stats.ships_left -= 1
+        #清空外星人和子弹
+        aliens.empty()
+        bullets.empty()
+        #创建一群新的外星人，并将飞船放到屏幕底端中央
+        create_fleet(ai_settings, screen, ship, aliens)
+        ship.center_ship()
+        #暂停
+        sleep(0.5)
+    else:
+        stats.game_active = False
+
+def check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets):
+    """检查是否有外星人到达了屏幕底端"""
+    screen_rect = screen.get_rect()
+    for alien in aliens.sprites():
+        if alien.rect.bottom >= screen_rect.bottom:
+            #像飞船被撞到一样处理
+            ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+            break
+
+def update_aliens(ai_settings, stats, screen, ship, aliens, bullets):
     """检查是否有外星人位于屏幕边缘，并更新它们的位置"""
     check_fleet_edges(ai_settings, aliens)
     aliens.update()
+    # 检测外星人和飞船之间的碰撞
+    if pygame.sprite.spritecollideany(ship, aliens):
+        ship_hit(ai_settings, stats, screen, ship, aliens, bullets)
+    #检查是否有外星人到达屏幕底端
+    check_aliens_bottom(ai_settings, stats, screen, ship, aliens, bullets)
--- settings.py
+++ settings.py
@@ -9,6 +9,7 @@ class Settings():
         self.bg_color = (230, 230, 230)
         # 飞船的设置
         self.ship_speed_factor = 1.6
+        self.ship_limit = 3
         # 子弹的设置
         self.bullet_speed_factor = 1
         self.bullet_width = 300
@@ -17,6 +18,6 @@ class Settings():
         self.bullets_allowed = 10
         # 外星人设置
         self.alien_speed_factor = 1
-        self.fleet_drop_speed = 10
+        self.fleet_drop_speed = 100
         # fleet_direction为1表示向右，为-1表示向左
         self.fleet_direction = 1
