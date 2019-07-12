6f0c2b2d98ecbcb624b9d3150311c3076f4eae9a
wangdefa <249558764@qq.com>
2019-07-13 02:29:06 +0800

    等待记分模块到来

--- alien_invasion.py
+++ alien_invasion.py
@@ -5,6 +5,7 @@ from settings import Settings
 from game_stats import GameStats
 from ship import Ship
 from button import Button
+from scoreboard import Scoreboard
 import game_functions as gf
 
 
@@ -21,6 +22,7 @@ def run_game():
 
     # 创建一个用于存储游戏统计信息的实例
     stats = GameStats(ai_settings)
+    sb = Scoreboard(ai_settings, screen, stats)
 
     # 创建一艘飞船 一个用于储存子弹的编组 一个外星人编组
     ship = Ship(ai_settings, screen)
@@ -31,13 +33,14 @@ def run_game():
     gf.create_fleet(ai_settings, screen, ship, aliens)
 
     # 开始游戏主循环
+
     while True:
-        gf.check_events(ai_settings, screen, ship, bullets)
+        gf.check_events(ai_settings, screen, stats, sb, play_button, ship, aliens, bullets)
         if stats.game_active:
             ship.update()
-            gf.update_bullets(ai_settings, screen, ship, aliens, bullets)
-            gf.update_aliens(ai_settings, stats, screen, ship, aliens, bullets)
-        gf.update_screen(ai_settings, screen, stats, ship, aliens, bullets, play_button)
+            gf.update_bullets(ai_settings, screen, stats, sb, ship, aliens, bullets)
+            gf.update_aliens(ai_settings, screen, stats, sb, ship, aliens, bullets)
+        gf.update_screen(ai_settings, screen, stats, sb, ship, aliens, bullets, play_button)
 
 
 run_game()
